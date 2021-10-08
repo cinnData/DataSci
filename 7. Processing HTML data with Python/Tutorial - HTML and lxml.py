17169aa03b7ba41eb54769f0e24aa69cf27f9082
cinnData <canela.miguel@icloud.com>
2021-10-08 18:45:18 +0200

    Add files via upload

---
+++ 7. Processing HTML data with Python/Tutorial - HTML and lxml.py
@@ -0,0 +1,35 @@
+## Tutorial - HTML and lxml ##
+
+# Parsing TML code #
+page = '<html> \
+  <head> \
+  <title>Data Viz</title> \
+  </head> \
+  <body> \
+  <div class="course" >Data Visualization</div> \
+  <div class="program">MBA full-time</div> \
+  <a class="professor", \
+  href="https://www.iese.edu/faculty-research/faculty/miguel-angel-canela">Miguel Ángel Canela</a> \
+  </body> \
+  </html>'
+page
+from lxml import html
+tree = html.fromstring(page)
+tree
+type(tree)
+tree[0]
+tree[1]
+tree[1][0]
+tree[1][0].text
+tree[1][0].attrib['class']
+
+# XPath expressions #
+tree.xpath('/html/body/div')
+tree.xpath('//div')
+tree.xpath('/html/body/div[@class="course"]')
+tree.xpath('//div/text()')
+tree.xpath('//div/text()')[0]
+tree.xpath('//div')[0].text
+tree.xpath('//a/@href')[0]
+tree.xpath('//a')[0].attrib['href']
+
