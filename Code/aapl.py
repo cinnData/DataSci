4c98d97d57a357b1349ab3d83947309ea624708d
cinnData <canela.miguel@icloud.com>
2023-06-22 17:48:52 +0200

    Update aapl.py

--- Code/aapl.py
+++ Code/aapl.py
@@ -8,7 +8,8 @@ df = pd.read_csv(filename, index_col=0, parse_dates=True)
 df.info()
 df.index
 df.index.name = None
-df['open'].plot(figsize=(10,6), color='black', linewidth=1);
+df.head()
+df['open'].plot(figsize=(8,5), color='black');
 
 # Q2. Data previous to January 15th #
 df[:'2019-01-14']
@@ -20,18 +21,18 @@ df[df.index.weekday == 4].head()
 
 # Q4. Distribution of daily returns #
 returns = df['open'].pct_change()
-returns
-returns.plot(figsize=(10,6), color='black', linewidth=1);
-returns.plot.hist(figsize=(8,6), color='gray', rwidth=0.98);
+returns.head()
+returns.plot(figsize=(7,5), color='black');
+returns.plot.hist(figsize=(7,5), color='gray', edgecolor='white');
 
 # Q5. Plot weekly average opening price #
 w_open = df['open'].resample('W').mean()
 w_open.head()
-w_open.plot(figsize=(10,6), color='black', linewidth=1);
+w_open.plot(figsize=(8,5), color='black');
 
 # Q6. 5-day moving average trend #
 df['open'].rolling(5).mean().head(10)
 ma = df['open'].rolling(5, center=True).mean()
 ma.head(10)
-ma.plot(figsize=(10,6), color='black', linewidth=1)
-df['open'].plot(color='gray', linestyle='--', linewidth=1);
+ma.plot(figsize=(8,5), color='black', linestyle='--')
+df['open'].plot(color='gray');
