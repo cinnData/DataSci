2bc7e4177f774305e6cb1ccdc6cba220a90c2bdd
cinnData <canela.miguel@icloud.com>
2023-08-23 13:41:19 +0200

    Update ds-02e.py

--- Code/ds-02e.py
+++ Code/ds-02e.py
@@ -1,10 +1,12 @@
-## Example - Amazon jobs ##
+## [DS-02E] Example - Amazon jobs ##
 
-# Importing the data (edit path) #
+# Importing the data #
 import pandas as pd
 path = 'https://raw.githubusercontent.com/cinnData/DataSci/main/Data/'
 filename = path + 'amzn.csv.zip'
 df = pd.read_csv(filename, index_col=0)
+
+# Exploring the data #
 df.info()
 df.head()
 
@@ -20,34 +22,17 @@ df['location'].value_counts().head(10)
 
 # Q2b. Positions in India #
 df['location'][df['location'].str[:2] == 'IN'].value_counts()
-df['location'][df['location'].str.startswith('IN')].value_counts()
-df['location'][df['location'].str.contains('^IN')].value_counts()
-
-# Q2c. Positions incomplete #
-(df['location'].str.count(',') == 2).mean().round(3)
-df.location[df['location'].str.count(',') == 1].value_counts().head(10)
-df.location[df['location'].str.count(',') == 0].value_counts()
+df['location'][df['location'].str.contains('^IN', regex=True)].value_counts()
 
 # Q3a. Programming languages in the basic qualifications field #
-df['basic_qualifications'].str.contains('java', case=False).mean().round(3)
-df['basic_qualifications'].str.contains('javascript', case=False).mean().round(3)
-df['basic_qualifications'].str.contains('java script', case=False).mean().round(3)
 df['basic_qualifications'].str.contains('c#', case=False).mean().round(3)
-df['basic_qualifications'].str.contains('c\+', case=False, regex=True).mean().round(3)
 df['basic_qualifications'].str.contains('c+', case=False, regex=False).mean().round(3)
-df['basic_qualifications'].str.contains('c+', case=False, regex=True).mean().round(3)
 df['basic_qualifications'].str.contains('c+', case=False).mean().round(3)
-df['basic_qualifications'].str.contains('sql', case=False).mean().round(3)
-df['basic_qualifications'].str.contains('mysql', case=False).mean().round(3)
-df['basic_qualifications'].str.contains('python', case=False).mean().round(3)
+df['basic_qualifications'].str.contains('c\+', case=False).mean().round(3)
 
-# Q3b. Programming languages in basic qualifications (alternative) #
+# Q3b. Programming languages in the basic qualifications field (alt) #
 bags = df['basic_qualifications'].str.lower().str.findall('\w+\+*')
-bags
-def f(x): return 'java' in x
-bags.apply(f).mean().round(3)
-def g(x): return 'c++' in x
-bags.apply(g).mean().round(3)
+bags.head()
 
 # Q4. Experience in preferred qualifications #
 df['preferred_qualifications'].str.contains('experience', case=False).mean().round(3)
