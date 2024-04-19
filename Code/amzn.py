5f6255145c401ed254622ce1eaa443a7798733e2
cinnData <canela.miguel@icloud.com>
2024-04-19 11:57:41 +0200

    Update amzn.py

--- Code/amzn.py
+++ Code/amzn.py
@@ -16,27 +16,16 @@ df.index.duplicated().sum()
 df = df.drop_duplicates()
 df.shape
 
-# Q2a. Top-ten locations for software developers at Amazon #
+# Q2. Top-ten locations for software developers at Amazon #
 df['location'].value_counts().shape
 df['location'].value_counts().head(10)
 
-# Q2b. Positions in India #
+# Q3. Positions in India #
 df['location'][df['location'].str[:2] == 'IN'].value_counts()
 df['location'][df['location'].str.contains('^IN', regex=True)].value_counts()
 
-# Q3a. Programming languages in the basic qualifications field #
+# Q4. Programming languages in the basic qualifications field #
 df['basic_qualifications'].str.contains('c#', case=False).mean().round(3)
 df['basic_qualifications'].str.contains('c+', case=False, regex=False).mean().round(3)
 df['basic_qualifications'].str.contains('c+', case=False).mean().round(3)
 df['basic_qualifications'].str.contains('c\+', case=False).mean().round(3)
-
-# Q3b. Programming languages in the basic qualifications field (alt) #
-bags = df['basic_qualifications'].str.lower().str.findall('\w+\+*')
-bags.head()
-
-# Q4. Experience in preferred qualifications #
-df['preferred_qualifications'].str.contains('experience', case=False).mean().round(3)
-df['preferred_qualifications'].str.count('\+?[0-9]\+? years').value_counts()
-df[df['preferred_qualifications'].str.count('[0-9]\+? years')> 5]
-df['preferred_qualifications'][1542577]
-df['preferred_qualifications'][1588664]
